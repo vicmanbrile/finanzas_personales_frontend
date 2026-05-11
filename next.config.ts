5bb9ca55b0785931a190b031876eb9a0b9c9614b
Víctor Briseño <victor.briseno.leon@hotmail.com>
2026-05-11 07:48:41 +0000

    standalone deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   allowedDevOrigins: ['127.0.0.1', 'localhost', '192.168.100.141'],
 
   async rewrites() {
